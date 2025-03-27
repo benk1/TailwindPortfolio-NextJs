9b00867320e99427f67b64b485f805510d8c374c
ben <kakengi_bernard@hotmail.com>
2025-03-27 13:20:12 +0200

    add Head Tag in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 import profilePic from '../public/assets/images/profile_pic.jpg';
 import Image from 'next/image';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>	
 			<Navbar />
 			<Component {...pageProps} />
 		</>
