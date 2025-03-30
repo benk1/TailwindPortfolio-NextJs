dccbf35c6433f8114228c307914a46e98f6997e0
ben <kakengi_bernard@hotmail.com>
2025-03-30 22:47:35 +0300

    fix the layout on different browsers

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,12 +9,14 @@ const Main = () => {
 	return (
 		<div id="home" className="w-full h-screen text-center">
 			{/* <div className="max-w-[1240px] w-full h-full mx-auto  p-2 flex justify-center items-center"> */}
-			<div className="flex flex-col items-center justify-center w-full max-w-screen-lg min-h-screen mx-auto text-center">
+			{/* <div className="flex flex-col items-center justify-center w-full max-w-screen-lg min-h-screen px-4 mx-auto text-center"> */}
+			<div className="flex flex-col items-center justify-center w-full max-w-screen-lg px-4 pt-10 pb-20 mx-auto text-center">
 				{/* <div className="mb-64 md:mb-44 md:mt-0"> */}
 				{/* <div className="mt-10 md:mt-0 "> */}
 				<div>
 					{/* <p className="text-sm tracking-widest text-gray-600 uppercase mt-44 md:mt-0"> */}
-					<p className="mt-10 text-sm tracking-widest text-gray-600 uppercase md:mt-0">
+					{/* <p className="mt-10 text-sm tracking-widest text-gray-600 uppercase md:mt-0"> */}
+					<p className="mb-4 text-sm tracking-widest text-gray-600 uppercase">
 						lets build something together
 					</p>
 					<h1 className="py-4 text-gray-700">
@@ -33,11 +35,13 @@ const Main = () => {
 							className="object-cover rounded-full w-36 h-36 sm:w-40 sm:h-40"
 							width={300}
 							height={300}
+							alt="Profile picture of Ben"
 						/>
 					</div>
 
 					<h1 className="py-4 text-gray-700">A web developer</h1>
-					<p className=" text-gray-600 max-w-[70%] m-auto">
+					{/* <p className=" text-gray-600 max-w-[70%] m-auto"> */}
+					<p className="text-gray-600 max-w-[90%] sm:max-w-[70%] text-sm sm:text-base mx-auto">
 						{' '}
 						I’m focused on building responsive front-end and modern web
 						applications
